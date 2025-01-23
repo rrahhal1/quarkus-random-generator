ead6745bf02ca2d57483cb39d6ccf7a2fbd21665
Rea Rahal <R.Rahal02@midisglobal.net>
2025-01-23 15:54:10 +0200

    Some new changes

--- src/main/java/com/redhat/developers/demos/quarkus/Main.java
+++ src/main/java/com/redhat/developers/demos/quarkus/Main.java
@@ -6,7 +6,7 @@ import io.quarkus.runtime.annotations.QuarkusMain;
 @QuarkusMain
 public class Main {
     public static void main(String... args) {
-        System.out.println("Starting the Quarkus capplication...");
+        System.out.println("Starting the Quarkus cappgergregregrregrelication...");
         Quarkus.run(args);
     }
 }
--- src/main/java/com/redhat/developers/demos/quarkus/service/RandomGeneratorService.java
+++ src/main/java/com/redhat/developers/demos/quarkus/service/RandomGeneratorService.java
@@ -7,6 +7,6 @@ public class RandomGeneratorService {
   private static final UUID id = UUID.randomUUID();
 
   public RandomResponse createRandomMessage() {
-    return new RandomResponse(id.toString()+"----Rea H Rahhal----*");
+    return new RandomResponse(id.toString()+"----Rea H Rahhal 1993----*");
   }
 }
