892bb56113bbc18b9ca36c9422bcbc3465346555
Rea Rahal <R.Rahal02@midisglobal.net>
2025-01-23 17:42:47 +0200

    Some new changes 0 7 afdsfdsfdsfs

--- src/main/java/com/redhat/developers/demos/quarkus/service/RandomGeneratorService.java
+++ src/main/java/com/redhat/developers/demos/quarkus/service/RandomGeneratorService.java
@@ -7,6 +7,6 @@ public class RandomGeneratorService {
   private static final UUID id = UUID.randomUUID();
 
   public RandomResponse createRandomMessage() {
-    return new RandomResponse(id.toString()+"----Rea H Rahhal 20 7 1993 bla bl---*");
+    return new RandomResponse(id.toString()+"----Rea H Rahhal 20 7 1993 bla blssssss---*");
   }
 }
