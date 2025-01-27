4ad283ca3bae3d3faf57a8ec26f8a6cdcbd1ca2f
Rea Rahal <R.Rahal02@midisglobal.net>
2025-01-27 16:05:02 +0200

    bla sssh

--- src/main/java/com/redhat/developers/demos/quarkus/service/RandomGeneratorService.java
+++ src/main/java/com/redhat/developers/demos/quarkus/service/RandomGeneratorService.java
@@ -7,6 +7,6 @@ public class RandomGeneratorService {
   private static final UUID id = UUID.randomUUID();
 
   public RandomResponse createRandomMessage() {
-    return new RandomResponse(id.toString()+"   blank boom clap space ");
+    return new RandomResponse(id.toString()+"    boom clap space ");
   }
 }
