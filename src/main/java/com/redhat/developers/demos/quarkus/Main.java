dddcc4d39a202805a3a6ba2f1c12be52a3a1702b
Rea Rahal <R.Rahal02@midisglobal.net>
2025-01-31 00:10:29 +0200

    frefr

--- src/main/java/com/redhat/developers/demos/quarkus/Main.java
+++ src/main/java/com/redhat/developers/demos/quarkus/Main.java
@@ -6,7 +6,7 @@ import io.quarkus.runtime.annotations.QuarkusMain;
 @QuarkusMain
 public class Main {
     public static void main(String... args) {
-        System.out.println("Starting the Quarkus    ddddegrregrelication...");
+        System.out.println("Starting the Quarkus    dddregrelication...");
         Quarkus.run(args);
     }
 }
