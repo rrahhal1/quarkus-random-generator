9fa1144457a75089fc2286ac9628d29623f84f0c
Rea Rahal <R.Rahal02@midisglobal.net>
2025-01-30 20:15:38 +0200

    fref

--- src/main/java/com/redhat/developers/demos/quarkus/service/RandomGeneratorService.java
+++ src/main/java/com/redhat/developers/demos/quarkus/service/RandomGeneratorService.java
@@ -7,6 +7,6 @@ public class RandomGeneratorService {
   private static final UUID id = UUID.randomUUID();
 
   public RandomResponse createRandomMessage() {
-    return new RandomResponse(id.toString()+"weefsfds-"+id.toString());
+    return new RandomResponse(id.toString()+"weefsxsxsfds-"+id.toString());
   }
 }
