d4a67e7469b341de89b5e7b84cf26e4cda9453da
Rea Rahal <R.Rahal02@midisglobal.net>
2025-01-31 10:57:38 +0200

    ffdfdfdew

--- src/main/java/com/redhat/developers/demos/quarkus/rest/RandomGeneratorResource.java
+++ src/main/java/com/redhat/developers/demos/quarkus/rest/RandomGeneratorResource.java
@@ -2,13 +2,11 @@ package com.redhat.developers.demos.quarkus.rest;
 
 import com.redhat.developers.demos.quarkus.service.RandomGeneratorService;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
-
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/random")
 public class RandomGeneratorResource {
