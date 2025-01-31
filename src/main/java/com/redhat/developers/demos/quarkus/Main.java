96d6aeee75908632a38ff66074ee943cf835e2d7
Rea Rahal <R.Rahal02@midisglobal.net>
2025-01-31 16:21:33 +0200

    ffccdfff.....dvd

--- src/main/java/com/redhat/developers/demos/quarkus/Main.java
+++ src/main/java/com/redhat/developers/demos/quarkus/Main.java
@@ -6,7 +6,7 @@ import io.quarkus.runtime.annotations.QuarkusMain;
 @QuarkusMain
 public class Main {
     public static void main(String... args) {
-        System.out.println("Starting the Quarku   fdsfdsfdsfdsfds...");
+        System.out.println("Starting the Quarku   fdsfdsfdsfdsfewfeefds...");
         Quarkus.run(args);
     }
 }
