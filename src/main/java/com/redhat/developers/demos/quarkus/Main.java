70713f4cfce6bfb7cbe1b1f34ce1943c59eb7af8
Rea Rahal <R.Rahal02@midisglobal.net>
2025-02-01 21:36:01 +0200

    jiejforfrf

--- src/main/java/com/redhat/developers/demos/quarkus/Main.java
+++ src/main/java/com/redhat/developers/demos/quarkus/Main.java
@@ -6,7 +6,7 @@ import io.quarkus.runtime.annotations.QuarkusMain;
 @QuarkusMain
 public class Main {
     public static void main(String... args) {
-        System.out.println("Starting the Qudsfdfdsarku  ...");
+        System.out.println("Starting the Qudsfddddssfdsarku  ...");
         Quarkus.run(args);
     }
 }
