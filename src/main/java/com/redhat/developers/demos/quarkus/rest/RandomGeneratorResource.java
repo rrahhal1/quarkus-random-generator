69e0a3b3944dafee73d9d029a33574a41f0f9841
Rea Rahal <R.Rahal02@midisglobal.net>
2025-02-02 15:27:21 +0200

    gtrgtmmdKff

--- src/main/java/com/redhat/developers/demos/quarkus/rest/RandomGeneratorResource.java
+++ src/main/java/com/redhat/developers/demos/quarkus/rest/RandomGeneratorResource.java
@@ -22,7 +22,7 @@ public class RandomGeneratorResource {
     @Consumes(MediaType.APPLICATION_JSON)
     public Response handleWebhook(String payload) {
         System.out.println("Received Webhook: " + payload);
-        return Response.ok().build();  //  200 OKffffdsfdffdfdfddscdsdfd to GitHub
+        return Response.ok().build();  //  200 OKffffdsfdffdfddscdsdfd to GitHub
     }
 
 }
