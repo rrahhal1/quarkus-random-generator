f8ab7f5333c20273036d82a45ea7f953109fc67a
Rea Rahal <R.Rahal02@midisglobal.net>
2025-02-02 10:11:06 +0200

    gtrgtrgtfffgff

--- src/main/java/com/redhat/developers/demos/quarkus/rest/RandomGeneratorResource.java
+++ src/main/java/com/redhat/developers/demos/quarkus/rest/RandomGeneratorResource.java
@@ -2,13 +2,15 @@ package com.redhat.developers.demos.quarkus.rest;
 
 import com.redhat.developers.demos.quarkus.service.RandomGeneratorService;
 
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
-@Path("/random")
+@Path("/")//was random
 public class RandomGeneratorResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
@@ -16,4 +18,11 @@ public class RandomGeneratorResource {
         RandomGeneratorService randomGeneratorService = new RandomGeneratorService();
         return Response.ok(randomGeneratorService.createRandomMessage()).build();
     }
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response handleWebhook(String payload) {
+        System.out.println("Received Webhook: " + payload);
+        return Response.ok().build();  // Return 200 OK to GitHub
+    }
+
 }
